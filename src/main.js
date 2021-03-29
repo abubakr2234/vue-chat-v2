fc3897403af1305066402c98d5552a4659857ff4
abubakr2234 <68422152+abubakr2234@users.noreply.github.com>
2021-03-29 23:07:25 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,35 @@
+
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import Routes from './routes'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Toasted from 'vue-toasted';
+import './assets/styles.css'
+
+library.add(faPencilAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Toasted)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.config.productionTip = false
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+})
+
+
+new Vue({
+  el: '#app',
+  components: { App },
+  router: router,
+  template: '<App/>'
+})
---
+++ src/serviecs/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCKJQwd2BGLowq7sXPa0bWaCZ7QsDQv4-8",
+    authDomain: "chatapp55-c50f6.firebaseapp.com",
+    projectId: "chatapp55-c50f6",
+    storageBucket: "chatapp55-c50f6.appspot.com",
+    messagingSenderId: "289468746342",
+    appId: "1:289468746342:web:08dadfd9514dff577c7ba7",
+    measurementId: "G-MLE9NCN8EL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+export default firebase;
